1f7b73a1d9dbd7007193145200a3f9e811f92fda
Sean Gillis <seangillis@Seans-MacBook-Pro.local>
2022-06-27 17:53:37 -0400

    user scores mapping well

--- src/components/UserScores.js
+++ src/components/UserScores.js
@@ -11,9 +11,9 @@ const UserScores = ({user}) => {
                     return (
                         <div className='user-scores' key={user.id}>
                             <h4>Recent scores for {thisUser.email}: </h4>
-                            {thisUser.scores <= 1 ? <h4>{thisUser.scores}</h4> : <h4>{thisUser.scores.sort().reverse().toString().replaceAll(',', ' || ')}</h4> }
+                            {/* {thisUser.scores <= 1 ? <h4>{thisUser.scores}</h4> : <h4>{thisUser.scores.sort().reverse().toString().replaceAll(',', ' || ')}</h4> } */}
                             <ul>
-                               {thisUser.scores.map((score)=>{
+                               {thisUser.scores.sort().reverse().map((score)=>{
                                    return (
                                    <li>{score}</li>
                                    )
